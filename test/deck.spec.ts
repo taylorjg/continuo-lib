8c3399cd9f77b091ba6cd4f0804c968892d544a5
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-06-16 22:45:05 +0100

    Setup jest and add first couple of unit tests

---
+++ test/deck.spec.ts
@@ -0,0 +1,15 @@
+import { Deck } from '../src/deck'
+
+describe('Deck tests', () => {
+
+  it('numCardsLeft after construction', () => {
+    const deck = new Deck()
+    expect(deck.numCardsLeft).toBe(42)
+  })
+
+  it('numCardsLeft after calling nextCard()', () => {
+    const deck = new Deck()
+    deck.nextCard()
+    expect(deck.numCardsLeft).toBe(41)
+  })
+})
